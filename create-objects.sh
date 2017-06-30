327579b85ccc4cb8207713cfe3503f85dda9ae5d
Samuel Tauil <samueltauil@gmail.com>
2017-06-30 12:26:10 -0400

    fixing typo

--- create-objects.sh
+++ create-objects.sh
@@ -5,5 +5,6 @@ oc create -f bc-welcome.json
 oc create -f dc-welcome.json
 oc create -f svc-welcome.json
 oc create -f route-welcome.json
+oc secrets new-dockercfg docker-registry --docker-server=docker-registry-default.apps.ocp.tauil.org --docker-username=admin --docker-password=$(oc whoami -t) --docker-email=test@test.com
 oc create -f pipeline/pipeline-inspector.yml
 oc create -f image-inspector-template.json
