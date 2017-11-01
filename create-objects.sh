c67f788e8a4a62adc96cf4896bc8b8a7ef4c06d9
Samuel Tauil <samueltauil@gmail.com>
2017-11-01 12:54:37 -0400

    adding oc client plugin during build

--- create-objects.sh
+++ create-objects.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+oc project demo
+oc adm policy add-scc-to-user privileged -z default -n demo
 
 oc create -f is-welcome.json
 oc create -f bc-welcome.json
@@ -8,3 +10,4 @@ oc create -f route-welcome.json
 oc secrets new-dockercfg docker-registry --docker-server=docker-registry-default.apps.ocp.tauil.org --docker-username=admin --docker-password=$(oc whoami -t) --docker-email=test@test.com
 oc create -f pipeline/pipeline-inspector.yml
 oc create -f image-inspector-template.json
+oc set env dc/jenkins INSTALL_PLUGINS=openshift-client:1.0.1
