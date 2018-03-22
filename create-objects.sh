240961e88f1aabd1414c6c0d50819af7a02918e1
Samuel Tauil <samueltauil@gmail.com>
2018-03-22 12:49:48 -0400

    refresh

--- create-objects.sh
+++ create-objects.sh
@@ -5,10 +5,12 @@ oc adm policy add-scc-to-user privileged -z default -n demo
 oc create -f is-welcome.json
 oc create -f bc-welcome.json
 oc create -f dc-welcome.json
+#oc create -f dc-jenkins.json
 oc set triggers dc/welcome --manual
 oc create -f svc-welcome.json
 oc create -f route-welcome.json
-oc secrets new-dockercfg docker-registry --docker-server=docker-registry-default.apps.ocp.tauil.org --docker-username=admin --docker-password=$(oc whoami -t) --docker-email=test@test.com
+#oc secrets new-dockercfg docker-registry --docker-server=docker-registry-default.apps.ocp.tauil.org --docker-username=admin --docker-password=$(oc whoami -t) --docker-email=test@test.com
+oc secrets new-dockercfg docker-registry --docker-server=docker-registry-default.apps.ocp.cloudvillage.in --docker-username=ocpadmin --docker-password=$(oc whoami -t) --docker-email=test@test.com
 oc create -f pipeline/pipeline-inspector.yml
 oc create -f image-inspector-template.json
 oc set env dc/jenkins INSTALL_PLUGINS=openshift-client:1.0.1
